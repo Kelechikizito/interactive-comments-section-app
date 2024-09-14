17f835673ca04708604393a5fc82d300d4fbab11
Kelechikizito <kaylaychi77@gmail.com>
2024-09-14 08:45:48 +0100

    files-update

--- src/App.js
+++ src/App.js
@@ -1,74 +1,33 @@
-import React, { useReducer } from "react";
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "./@/components/ui/carousel";
-
-function formReducer(state, action) {
-  switch (action.type) {
-    case "updateField":
-      return {
-        ...state,
-        [action.field]: action.value,
-      };
-    case "reset":
-      return {
-        name: "",
-        email: "",
-      };
-    default:
-      return state;
-  }
-}
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 export default function App() {
-  const initialState = { name: "", email: "" };
-  const [state, dispatch] = useReducer(formReducer, initialState);
-
-  const handleInputChange = (e) => {
-    dispatch({
-      type: "updateField",
-      field: e.target.name,
-      value: e.target.value,
-    });
-  };
-
   return (
-    <div>
-      <form>
-        <label>
-          Name:
-          <input
-            type="text"
-            name="name"
-            value={state.name}
-            onChange={handleInputChange}
-          />
-        </label>
-        <br />
-        <label>
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={state.email}
-            onChange={handleInputChange}
-          />
-        </label>
-      </form>
-      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
-      <Carousel>
-        <CarouselContent>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>...</CarouselItem>
-          <CarouselItem>...</CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
-    </div>
-  );
+    <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>Is it accessible?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It adheres to the WAI-ARIA design pattern.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Is it styled?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It comes with default styles that matches the other
+          components&apos; aesthetic.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>Is it animated?</AccordionTrigger>
+        <AccordionContent>
+          Yes. It&apos;s animated by default, but you can disable it if you
+          prefer.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
+  )
 }
