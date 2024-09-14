e59edaee4f5518c30e3693dcd5fa8abf1b3a9925
Kelechikizito <kaylaychi77@gmail.com>
2024-09-14 08:19:15 +0100

    file update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./@/components/ui/carousel";
 
-function App() {
+function formReducer(state, action) {
+  switch (action.type) {
+    case "updateField":
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case "reset":
+      return {
+        name: "",
+        email: "",
+      };
+    default:
+      return state;
+  }
+}
+
+export default function App() {
+  const initialState = { name: "", email: "" };
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
+  const handleInputChange = (e) => {
+    dispatch({
+      type: "updateField",
+      field: e.target.name,
+      value: e.target.value,
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={state.name}
+            onChange={handleInputChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={state.email}
+            onChange={handleInputChange}
+          />
+        </label>
+      </form>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </div>
   );
 }
-
-export default App;
