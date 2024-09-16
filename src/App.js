cf2124926525e7fe516ab6af4af9f838daa1e754
Kelechikizito <kaylaychi77@gmail.com>
2024-09-16 15:36:30 +0100

    files-update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import ModalPopup from "Components/ModalPopup"
+import ModalPopup from "components/ModalPopup";
+import Maincontainer from "components/MainContainer";
 
 const App = () => {
   return (
     <div>
       <ModalPopup />
+      <Maincontainer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Components/MainContainer.js
@@ -0,0 +1,9 @@
+import './MainContainer.css';
+
+const Maincontainer = () => {
+  return (
+    <div className='main-container'>Maincontainer</div>
+  )
+}
+
+export default Maincontainer;
