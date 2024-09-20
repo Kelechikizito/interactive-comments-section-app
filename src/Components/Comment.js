36ac6686a19b93074d241529b6cfd1c4ad32c2e0
Kelechikizito <kaylaychi77@gmail.com>
2024-09-20 10:22:59 +0100

    files update

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -1,9 +1,33 @@
-import './Comment.css'
+import "./Comment.css";
+import { useState, useEffect } from "react";
 
 const Comment = () => {
-  return (
-    <div>Comment</div>
-  )
-}
+  const [data, setData] = useState(null);
 
-export default Comment
+  useEffect(() => {
+    // If you're using Create React App and the file is in the public folder
+    fetch("/data.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setData(data))
+      .catch((error) =>
+        console.error(
+          "There has been a problem with your fetch operation:",
+          error
+        )
+      );
+  }, []);
+
+  return (
+    <div>
+      {/* Render your data here */}
+      {<p>{JSON.stringify(data, null, 2)}</p>}
+    </div>
+  );
+};
+
+export default Comment;
