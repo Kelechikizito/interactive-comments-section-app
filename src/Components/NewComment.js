6ad68687aaec1789e17d36e1d9ea87650ce217fe
Kelechikizito <kaylaychi77@gmail.com>
2024-10-09 18:30:56 +0100

    files-update

--- src/Components/NewComment.js
+++ src/Components/NewComment.js
@@ -6,42 +6,65 @@ import JuliusOmoAvatar from "../images/avatars/image-juliusomo.png";
 
 const NewComment = (props) => {
   const [textareaValue, setTextareaValue] = useState("");
+  const [createdDivs, setCreatedDivs] = useState([]);
 
   const handleTextareaChange = (e) => {
-    setTextareaValue(e.target.value)
-  }
+    setTextareaValue(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(textareaValue)
+    console.log(textareaValue);
     if (textareaValue.trim()) {
-    //   setCreatedDivs([...createdDivs, textareaValue])
-      setTextareaValue('')
+      setCreatedDivs([...createdDivs, textareaValue]);
+      setTextareaValue("");
     }
-  }
+  };
 
   // localStorage.setItem('commentReplies', JSON.stringify(props.comment))
   // console.log(JSON.parse(localStorage.getItem('commentReplies')))
 
   return (
-    <div className="bg-white rounded-xl p-4 flex justify-between gap-4">
-      <img src={JuliusOmoAvatar} alt="JuliusOmoAvatar" className="size-8" />
+    <div className="flex flex-col gap-6">
+      <div className="order-last bg-white rounded-xl p-4 flex justify-between gap-4">
+        <img src={JuliusOmoAvatar} alt="JuliusOmoAvatar" className="size-8" />
 
-      <form className="flex justify-between w-full gap-4" onSubmit={handleSubmit}>
-        <Textarea
-          placeholder="Add a comment..."
-          className="h-24 focus:border-black focus:border-2 focus:outline-2"
-          value={textareaValue}
-          onChange={handleTextareaChange}
-        />
-        <Button
-          className="px-6 py-5 font-medium"
-          style={{ backgroundColor: "hsl(238, 40%, 52%)" }}
-          type="submit"
+        <form
+          className="flex justify-between w-full gap-4"
+          onSubmit={handleSubmit}
         >
-          SEND
-        </Button>
-      </form>
+          <Textarea
+            placeholder="Add a comment..."
+            className="h-24 focus:border-black focus:border-2 focus:outline-2"
+            value={textareaValue}
+            onChange={handleTextareaChange}
+          />
+          <Button
+            className="px-6 py-5 font-medium"
+            style={{ backgroundColor: "hsl(238, 40%, 52%)" }}
+            type="submit"
+          >
+            SEND
+          </Button>
+        </form>
+      </div>
+
+      {createdDivs.map((text, index) => (
+        <CommentReply
+          key={index}
+          image={JuliusOmoAvatar}
+          imageUsername="juliusomo"
+          username="juliusomo"
+          comment={text}
+          createdDate="Now"
+          replyingTo="ll"
+          vote={0}
+          isLastReply={true}
+        />
+        // <div key={index} className="p-4 bg-gray-100 rounded-md">
+        //   {text}
+        // </div>
+      ))}
     </div>
   );
 };
