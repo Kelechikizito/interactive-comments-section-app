e8cab0d24a83291032bd121fa6428b26b1a8c381
Kelechikizito <kaylaychi77@gmail.com>
2024-10-09 16:45:22 +0100

    files-update

--- src/Components/NewComment.js
+++ src/Components/NewComment.js
@@ -1,19 +1,30 @@
-import { Textarea } from "../@/components/ui/textarea"
-import { Button } from "../@/components/ui/button"
+import { useState } from "react";
+import { Textarea } from "../@/components/ui/textarea";
+import { Button } from "../@/components/ui/button";
 import JuliusOmoAvatar from "../images/avatars/image-juliusomo.png";
 
-
 const NewComment = (props) => {
-    // localStorage.setItem('commentReplies', JSON.stringify(props.comment))
-    // console.log(JSON.parse(localStorage.getItem('commentReplies')))
+  // localStorage.setItem('commentReplies', JSON.stringify(props.comment))
+  // console.log(JSON.parse(localStorage.getItem('commentReplies')))
 
   return (
     <div className="bg-white rounded-xl p-4 flex justify-between gap-4">
       <img src={JuliusOmoAvatar} alt="JuliusOmoAvatar" className="size-8" />
-      
+
       <div className="flex justify-between w-full gap-4">
-        <Textarea placeholder="Add a comment..." className='h-24 focus:border-black focus:border-2 focus:outline-2'/>
-        <Button className="px-6 py-5 font-medium" style={{backgroundColor: 'hsl(238, 40%, 52%)'}}>SEND</Button>
+        <Textarea
+          placeholder="Add a comment..."
+          className="h-24 focus:border-black focus:border-2 focus:outline-2"
+          value={textareaValue}
+          onChange={handleTextareaChange}
+        />
+        <Button
+          className="px-6 py-5 font-medium"
+          style={{ backgroundColor: "hsl(238, 40%, 52%)" }}
+          type="submit"
+        >
+          SEND
+        </Button>
       </div>
     </div>
   );
